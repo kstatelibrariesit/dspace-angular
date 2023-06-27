d4ac0dbe6b55529527e5c1de20223ac6ead40240
Thomas Misilo <tmisilo@ksu.edu>
2023-06-27 16:57:36 -0500

    K-REx theme based on dspace theme

---
+++ src/themes/krex/app/ksu-footer/ksu-footer.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+/**
+ * Simple component to inject the standard KSU Footer
+ */
+@Component({
+  selector: 'ds-ksu-footer',
+  styleUrls: ['./ksu-footer.component.scss'],
+  templateUrl: './ksu-footer.component.html',
+})
+
+export class KsuFooterComponent {
+}
