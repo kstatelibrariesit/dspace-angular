d893d3376aa1530bfb0965d3f2b41ca3b653029f
Thomas Misilo <tmisilo@ksu.edu>
2023-10-18 10:10:39 -0500

    Add Attachments Info Page

---
+++ src/app/info/guidelines/attachments/attachments.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ds-guidelines-attachments',
+  templateUrl: './attachments.component.html',
+  styleUrls: ['./attachments.component.scss']
+})
+/**
+ * Component displaying the Attachments page
+ */
+export class AttachmentsGuidelinesComponent {
+}
