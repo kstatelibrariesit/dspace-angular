4335556b9c2a313ebf965c65661d4368695c2ef7
Thomas Misilo <tmisilo@ksu.edu>
2023-10-19 08:52:30 -0500

    Add in the custom area below searchbar

--- src/themes/krex/lazy-theme.module.ts
+++ src/themes/krex/lazy-theme.module.ts
@@ -61,10 +61,12 @@ import { SharedBrowseByModule } from '../../app/shared/browse-by/shared-browse-b
 import { ItemVersionsModule } from '../../app/item-page/versions/item-versions.module';
 import { ItemSharedModule } from 'src/app/item-page/item-shared.module';
 import { HomePageComponent } from './app/home-page/home-page.component';
+import { KsuHomeBelowSearchBarComponent } from './app/home-page/ksu-home-below-search-bar/ksu-home-below-search-bar.component';
 
 
 const DECLARATIONS = [
   HomePageComponent,
+  KsuHomeBelowSearchBarComponent
 ];
 
 @NgModule({
