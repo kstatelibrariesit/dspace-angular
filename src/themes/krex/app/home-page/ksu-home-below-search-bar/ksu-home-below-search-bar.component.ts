9eaa3de05066a285e0ec22415f28636dec39d53b
Thomas Misilo <tmisilo@ksu.edu>
2023-10-19 08:52:30 -0500

    Add in the custom area below searchbar

---
+++ src/themes/krex/app/home-page/ksu-home-below-search-bar/ksu-home-below-search-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ds-ksu-home-below-search-bar',
+  styleUrls: ['./ksu-home-below-search-bar.component.scss'],
+  templateUrl: './ksu-home-below-search-bar.component.html'
+})
+
+/**
+ * Component to render the news section on the home page
+ */
+export class KsuHomeBelowSearchBarComponent {}
